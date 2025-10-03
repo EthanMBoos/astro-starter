a44f7e232dd157e88aad3c0a3ee3ba822e57674a
Ethan Boos <moosboos1@gmail.com>
2025-10-03 15:11:30 -0400

    Delete ccls.lua

--- lua/plugins/ccls.lua
+++
@@ -1,6 +0,0 @@
--- ~/.config/nvim/lua/user/plugins/ccls.lua
-return {
-  "ranjithshegde/ccls.nvim",
-  ft = { "c", "cpp", "objc", "objcpp", "cuda" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-}
